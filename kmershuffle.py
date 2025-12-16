dc4f151b58136f9995d95bc60c8f34e971f3c651
Mathis Wülpern <mathiswuelpern@gmail.com>
2025-12-16 13:12:38 +0100

    Update kmershuffle.py

--- kmershuffle.py
+++ kmershuffle.py
@@ -4,6 +4,8 @@ import pybedtools
 import numpy as np
 import pandas as pd
 import argparse
+import tempfile
+import os
 
 parser = argparse.ArgumentParser(description='Z-scoring of shuffled k-mers')
 
@@ -15,8 +17,14 @@ parser.add_argument('--shufflings', action="store", dest='shufflings', default=1
 
 args = parser.parse_args()
 gff = args.anno
+if not os.path.exists(gff):
+    raise FileNotFoundError("GTF-file does not exist")
 bed = args.binds
+if not os.path.exists(bed):
+    raise FileNotFoundError("BED-file does not exist")
 fasta = args.seq
+if not os.path.exists(fasta):
+    raise FileNotFoundError("FASTA-file does not exist")
 k = int(args.k)
 shufflings = int(args.shufflings)
 
@@ -25,13 +33,12 @@ def count_kmers(seqs, k):
      kmers = {}
      sum_seqs = 0
      for seq in seqs:
-        sum_seqs += 1
-        rna = seq.replace("T", "U").upper().strip()
-        for i in range(len(rna)-k+1):
-            if rna[i:k] not in kmers.keys():
-                kmers[rna[i:i+k]] = 0
+        # rna = seq.replace("T", "U").upper().strip()
+        for i in range(len(seq)-k+1):
+            if seq[i:k] not in kmers.keys():
+                kmers[seq[i:i+k]] = 0
 
-            kmers[rna[i:i+k]] += 1
+            kmers[seq[i:i+k]] += 1
      return kmers
 
 df = pd.read_table(gff, names=["chr",2,'feature',"start",'stop',6,"strand",8,'attr'])
@@ -52,30 +59,31 @@ bind_sites_small = []
 dd = defaultdict(list)
 kmers = {}
 seqs = bind_sites.sequence(fi=fasta)
-seqs.save_seqs("binding_features.fa")
-with pysam.FastxFile("binding_features.fa") as fh:
-    site_length = len(fh.__next__().sequence)
-    kmers = count_kmers([f.sequence for f in fh],k)
+with tempfile.TemporaryDirectory() as tmpdir:
+    seqs.save_seqs(f"{tmpdir}/binding_features.fa")
+    with pysam.FastxFile(f"{tmpdir}/binding_features.fa") as fh:
+        site_length = len(fh.__next__().sequence)
+        kmers = count_kmers([f.sequence for f in fh],k)
 
-for feature in bind_sites:
-    median = int(np.median(np.arange(feature.start, feature.stop)))
-    bind_sites_small.append((feature.chrom, median, median))
+    for feature in bind_sites:
+        median = int(np.median(np.arange(feature.start, feature.stop)))
+        bind_sites_small.append((feature.chrom, median, median))
 
-bind_features = pybedtools.BedTool.from_dataframe(genes[genes["gene"].str.fullmatch(r'(AT\dG\d{5})')==True]).intersect(bind_sites_small,u=True)
-count_features = bind_features.intersect(bind_sites_small, c=True)
+    bind_features = pybedtools.BedTool.from_dataframe(genes[genes["gene"].str.fullmatch(r'(AT\dG\d{5})')==True]).intersect(bind_sites_small,u=True)
+    count_features = bind_features.intersect(bind_sites_small, c=True)
 
-for shuffle in range(shufflings):
-    seqs = pybedtools.BedTool(list(np.concatenate([[(feature.chrom, site_start, site_start+site_length) for site_start in np.random.randint(feature.start, feature.stop+1, size=feature.count)] for feature in count_features]))).sequence(fi="TAIR10_chr_all.fa")
-    seqs.save_seqs(f"binding_features{shuffle}.fa")
-    with pysam.FastxFile(f"binding_features{shuffle}.fa") as fh:
-            shuffled_kmers = count_kmers([f.sequence for f in fh],k)
-            for key, value in shuffled_kmers.items():
-                dd[key].append(value)
+    for shuffle in range(shufflings):
+        print(f"Shuffle: {shuffle+1}")
+        seqs = pybedtools.BedTool(list(np.concatenate([[(feature.chrom, site_start, site_start+site_length) for site_start in np.random.randint(feature.start, feature.stop+1, size=feature.count)] for feature in count_features]))).sequence(fi="TAIR10_chr_all.fa")
+        seqs.save_seqs(f"{tmpdir}/binding_features{shuffle}.fa", )
+        with pysam.FastxFile(f"{tmpdir}/binding_features{shuffle}.fa") as fh:
+                shuffled_kmers = count_kmers([f.sequence for f in fh],k)
+                for key, value in shuffled_kmers.items():
+                    dd[key].append(value)
 
 kmer_stats = {}
 for key in kmers.keys():
-    kmer_stats[key] = [kmers[key],np.mean(dd[key]), np.std(dd[key]), (kmers[key]-np.mean(dd[key]))/np.std(dd[key])]
+    kmer_stats[key.replace("T","U")] = [kmers[key],np.mean(dd[key]), np.std(dd[key]), (kmers[key]-np.mean(dd[key]))/np.std(dd[key])]
 
 stats = pd.DataFrame.from_dict(kmer_stats, orient="index", columns=["count","mean shufflings", "std shufflings", "z-score"])
 stats.to_csv("kmer_stats.csv")
-
